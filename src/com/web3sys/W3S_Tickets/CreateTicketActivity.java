9d510ad61d3a887903383978f0de372d819eed11
Oscuro87 <capelle.e@gmail.com>
2014-04-01 13:43:06 +0200

    Passage de la page Login à la page create ticket

---
+++ src/com/web3sys/W3S_Tickets/CreateTicketActivity.java
@@ -0,0 +1,22 @@
+package com.web3sys.W3S_Tickets;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+/**
+ * Created by User on 1/04/14.
+ */
+public class CreateTicketActivity extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.createticket);
+    }
+}
