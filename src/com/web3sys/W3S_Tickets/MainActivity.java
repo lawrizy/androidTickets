5bf72b06528392b61dcfe4484f481e8f48559ac2
DesaedeleerL <shasnape06@gmail.com>
2014-04-01 10:20:38 +0200

    Mise à jour ajout spinner 2

--- src/com/web3sys/W3S_Tickets/MainActivity.java
+++ src/com/web3sys/W3S_Tickets/MainActivity.java
@@ -1,9 +1,12 @@
 package com.web3sys.W3S_Tickets;
-
+import android.content.Intent;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
     /**
@@ -17,7 +20,17 @@ public class MainActivity extends Activity {
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         setContentView(R.layout.main);
+        OnClickListener listnr=new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i= new Intent(getBaseContext(),CreateTicket.class);
+                startActivity(i);
+            }
+        };
+        Button btn =(Button) findViewById(R.id.button);
+        btn.setOnClickListener(listnr);
     }
+
 
     public void testExit()
     {
