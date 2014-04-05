6a455f03b3a06db3700ee7339e10a1426b82968e
Oscuro87 <capelle.e@gmail.com>
2014-04-05 12:50:46 +0200

    Implémentation des alertes dans les vues

--- src/activities/CreateTicketActivity.java
+++ src/activities/CreateTicketActivity.java
@@ -1,6 +1,7 @@
 package activities;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -9,7 +10,9 @@ import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 import com.web3sys.W3S_Tickets.R;
+import enums.ERROR_SET_MODE;
 
 public class CreateTicketActivity extends Activity{
 
@@ -47,7 +50,75 @@ public class CreateTicketActivity extends Activity{
 
     private void sendCreateTicket()
     {
-        Log.i("AndroidTickets", "Attempting to create a new ticket... (Not implemented)");
+        Log.i("AndroidTickets", "Attempting to create a new ticket...");
+        Log.i("AndroidTickets", "Validating fields...");
+        boolean validated = validateFields();
+        Log.i("AndroidTickets", "Field validation result: " + validated);
+    }
+
+    private void setErrorMessage(String message, ERROR_SET_MODE mode)
+    {
+        TextView errorArea = (TextView)findViewById(R.id.errorMessageTextView);
+        switch(mode)
+        {
+            case CLEAR_AND_CREATE:
+                clearErrorMessage();
+                errorArea.setText(message);
+                break;
+            case APPEND:
+                String oldMessage = "";
+                if(errorArea.getText() != null)
+                    oldMessage = errorArea.getText().toString();
+                String newMessage = oldMessage;
+                if(errorMessageExists())
+                    if(oldMessage.charAt(oldMessage.length()-1) != '\n')
+                        newMessage += "\n";
+                newMessage += message;
+                clearErrorMessage();
+                errorArea.setText(newMessage);
+                break;
+        }
+    }
+
+    private void clearErrorMessage()
+    {
+        TextView errorArea = (TextView)findViewById(R.id.errorMessageTextView);
+        errorArea.setText("");
+    }
+
+    private boolean errorMessageExists()
+    {
+        TextView errorArea = (TextView)findViewById(R.id.errorMessageTextView);
+        return (errorArea.getText() != null & errorArea.getText().length() > 0 & errorArea.getText().toString() != "") ? true : false;
+    }
+
+    private boolean validateFields() {
+        Context context = getApplicationContext();
+        Spinner catSpinner = (Spinner)findViewById(R.id.categorySpinner);
+        Spinner subCatSpinner = (Spinner)findViewById(R.id.subCategorySpinner);
+        Spinner buildingSpinner = (Spinner)findViewById(R.id.buildingSpinner);
+
+        boolean retVal = true;
+
+        if(errorMessageExists()) clearErrorMessage();
+
+        if(catSpinner.getSelectedItem() == null)
+        {
+            setErrorMessage(context.getString(R.string.errorCategoryRequired) + "\n", ERROR_SET_MODE.APPEND);
+            retVal = false;
+        }
+        if(subCatSpinner.getSelectedItem() == null)
+        {
+            setErrorMessage(context.getString(R.string.errorSubCategoryRequired) + "\n", ERROR_SET_MODE.APPEND);
+            retVal = false;
+        }
+        if(buildingSpinner.getSelectedItem() == null)
+        {
+            setErrorMessage(context.getString(R.string.errorBuildingRequired) + "\n", ERROR_SET_MODE.APPEND);
+            retVal = false;
+        }
+
+        return retVal;
     }
 
     private void resetAllFields()
