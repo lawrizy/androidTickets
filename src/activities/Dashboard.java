0d2b1c3516696f1365e802cda98e08addfd3b38b
Oscuro87 <capelle.e@gmail.com>
2014-04-05 11:02:12 +0200

    Update de tous les layouts - part 2

---
+++ src/activities/Dashboard.java
@@ -0,0 +1,14 @@
+package activities;
+
+import android.app.Activity;
+import android.os.Bundle;import com.web3sys.W3S_Tickets.R;
+
+/**
+ * Created by User on 5/04/14.
+ */
+public class Dashboard extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dashboard);
+    }
+}
