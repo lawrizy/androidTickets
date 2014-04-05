e4b7f14a0c0225c2852e5b75a464c399d012793c
Oscuro87 <capelle.e@gmail.com>
2014-04-05 12:57:42 +0200

    Update dashboard

--- src/activities/Dashboard.java
+++ src/activities/Dashboard.java
@@ -2,6 +2,7 @@ package activities;
 
 import android.app.Activity;
 import android.os.Bundle;import com.web3sys.W3S_Tickets.R;
+import org.achartengine.renderer.DefaultRenderer;
 
 /**
  * Created by User on 5/04/14.
@@ -10,5 +11,17 @@ public class Dashboard extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dashboard);
+
+        setupListeners();
+        setupGraphs();
+    }
+
+    private void setupGraphs() {
+        DefaultRenderer renderer = new DefaultRenderer();
+
+    }
+
+    private void setupListeners() {
+
     }
 }
