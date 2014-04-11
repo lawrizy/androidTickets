093fced2c72e851ed8968386630223ccfe90c924
Oscuro87 <capelle.e@gmail.com>
2014-04-11 12:49:18 +0200

    Test UserSessionInfo

---
+++ src/model/UserSessionInfo.java
@@ -0,0 +1,16 @@
+package model;
+
+/**
+ * Created by User on 11/04/2014.
+ */
+
+/**
+ * Cette classe stocke toutes les informations utiles pour pouvoir reconnaître l'utilisateur et ainsi l'identifier
+ * tout au long de la durée de l'application. (ex: envoi d'emails, recherche DB par rapport au numéro d'utilisateur, ...)
+ */
+public class UserSessionInfo
+{
+    public static int       USER_ID;
+    public static String    USER_EMAIL;
+    public static String    USER_NAME;
+}
